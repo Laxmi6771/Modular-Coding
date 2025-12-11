71d59d48e8c857299e35144bfc4742ef1517f347
unknown <Laxmi6771>
2025-12-11 17:04:25 +0530

    Initial commit

---
+++ footer.js
@@ -0,0 +1,7 @@
+export function footer() {
+    return `
+        <footer style="padding:10px; background:#222; color:#fff; text-align:center;">
+            <p>© 2025 Modular JS App</p>
+        </footer>
+    `;
+}
